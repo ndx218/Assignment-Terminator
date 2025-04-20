65642e2488eb82469b4d5733fe8ae95b8a35a0d5
ndx218 <ndx218@gmail.com>
2025-04-21 04:57:36 +0800

    Update recharge.tsx

--- pages/recharge.tsx
+++ pages/recharge.tsx
@@ -1,1 +1,9 @@
+export default function Recharge() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">💳 充值頁面</h1>
+      <p>這裡會顯示信用額度和充值記錄（待完成）</p>
+    </div>
+  );
+}
 // Recharge system page
