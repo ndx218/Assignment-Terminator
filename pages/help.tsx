49484fcc8d7176ddd26c00eb71d502dc095948a0
ndx218 <ndx218@gmail.com>
2025-04-21 04:57:57 +0800

    Update help.tsx

--- pages/help.tsx
+++ pages/help.tsx
@@ -1,1 +1,14 @@
+export default function Help() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">❓ 常見問題</h1>
+      <ul>
+        <li>Q: 如何生成作業？</li>
+        <li>A: 填寫資料後按生成即可。</li>
+        <li>Q: 如何充值？</li>
+        <li>A: 前往充值頁面即可。</li>
+      </ul>
+    </div>
+  );
+}
 // Help page content
