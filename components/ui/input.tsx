0114ea657fe1e3ba2ab1f4acf1023e1963fb44eb
ndx218 <ndx218@gmail.com>
2025-05-04 02:02:37 +0800

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,1 +1,15 @@
-// UI input component
+// UI input component// components/ui/input.tsx
+
+import React, { InputHTMLAttributes } from 'react';
+import { cn } from '@/lib/utils';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+}
+
+export const Input = ({ className = '', ...props }: InputProps) => (
+  <input
+    className={cn(`border px-2 py-1 rounded ${className}`)}
+    {...props}
+  />
+);
