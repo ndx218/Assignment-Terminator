33b52082854304c2d9a2fbf81a0011d81039caa5
ndx218 <ndx218@gmail.com>
2025-05-04 09:47:28 +0800

    Create auth.ts

---
+++ lib/auth.ts
@@ -0,0 +1,42 @@
+// lib/auth.ts
+import type { NextAuthOptions, Session } from 'next-auth';
+import GitHubProvider from 'next-auth/providers/github';
+import type { JWT } from 'next-auth/jwt';
+
+declare module 'next-auth' {
+  interface User {
+    id: string;
+  }
+  interface Session {
+    user: {
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+  }
+}
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({
+      session,
+      token,
+    }: {
+      session: Session;
+      token: JWT;
+    }) {
+      if (session.user && token.sub) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
+};
