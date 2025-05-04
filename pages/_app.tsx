b4a2f8e6893feed72b3b465c2ff7defde0501baf
ndx218 <ndx218@gmail.com>
2025-05-04 09:42:55 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 import Sidebar from '@/components/Sidebar'
 import PointBalance from '@/components/PointBalance'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <div className="flex min-h-screen">
-      <Sidebar />
-      <main className="flex-1 p-6 relative">
-        <Component {...pageProps} />
-        <PointBalance />
-      </main>
-    </div>
+    <SessionProvider session={session}>
+      <div className="flex min-h-screen">
+        <Sidebar />
+        <main className="flex-1 p-6 relative">
+          <Component {...pageProps} />
+          <PointBalance />
+        </main>
+      </div>
+    </SessionProvider>
   )
 }
