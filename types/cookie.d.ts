8d1970ba083a3db861cc7547d22b295cc9464682
ndx218 <ndx218@gmail.com>
2025-05-07 07:59:38 +0800

    Update cookie.d.ts

--- types/cookie.d.ts
+++ types/cookie.d.ts
@@ -1,5 +1,3 @@
-// types/cookie.d.ts
-
 declare module 'cookie' {
   export function parse(str: string): { [key: string]: string };
   export function serialize(name: string, val: string, options?: any): string;
