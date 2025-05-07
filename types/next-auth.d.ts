495911881dc0b1dca0b43222cdfb50380594b68c
ndx218 <ndx218@gmail.com>
2025-05-07 10:08:52 +0800

    Update next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,28 +1,25 @@
 // types/next-auth.d.ts
-import NextAuth, { DefaultSession } from "next-auth";
+import NextAuth from "next-auth";
 
 declare module "next-auth" {
-  interface Session {
-    user: {
-      id: string;
-      name?: string | null;      email?: string | null;
-      image?: string | null;
-      phone?: string | null;
-      referredBy?: string | null;
-      referralCode?: string | null;
-    } & DefaultSession["user"];
-  }
-
   interface User {
     id: string;
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
     phone?: string | null;
     referredBy?: string | null;
     referralCode?: string | null;
+  }
+
+  interface Session {
+    user: User;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
+    id: string;
     phone?: string | null;
     referredBy?: string | null;
     referralCode?: string | null;
