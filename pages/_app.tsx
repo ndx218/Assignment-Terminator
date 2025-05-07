1d13d9d382f401f2967762d2d5954b6d699391e8
ndx218 <ndx218@gmail.com>
2025-05-07 09:01:51 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { SessionProvider } from 'next-auth/react'
-import Sidebar from '@/components/Sidebar'
-import PointBalance from '@/components/PointBalance'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
+import Sidebar from '@/components/Sidebar';
+import PointBalance from '@/components/PointBalance';
+import { Toaster } from 'sonner'; // ✅ 加入 toast 彈窗
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
           <PointBalance />
         </main>
       </div>
+      <Toaster position="top-center" richColors /> {/* ✅ 彈出提示容器放在根層 */}
     </SessionProvider>
-  )
+  );
 }
