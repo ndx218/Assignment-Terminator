a14c4f63b320fd7651bc3b8976e03f90b971b888
ndx218 <ndx218@gmail.com>
2025-05-08 05:59:53 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
-import Sidebar from '@/components/Sidebar';
-import PointBalance from '@/components/PointBalance';
-import { Toaster } from 'sonner'; // ✅ 加入 toast 彈窗
+import { Toaster } from 'sonner';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <div className="flex min-h-screen">
-        <Sidebar />
-        <main className="flex-1 p-6 relative">
-          <Component {...pageProps} />
-          <PointBalance />
-        </main>
-      </div>
-      <Toaster position="top-center" richColors /> {/* ✅ 彈出提示容器放在根層 */}
+      <Component {...pageProps} />
+      <Toaster position="top-center" richColors />
     </SessionProvider>
   );
 }
