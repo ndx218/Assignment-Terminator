6490cd0dea73ddf25d94af5ac03591ca9519143a
ndx218 <ndx218@gmail.com>
2025-05-08 10:24:26 +0800

    Delete pages/index.tsx

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-// pages/index.tsx
-export { default } from '@/components/ui/EasyWorkUI';
-
-export const getServerSideProps = () => {
-  return {
-    props: {}, // 避免 Next.js 預先靜態生成，確保 useSession() 僅在 client 執行
-  };
-};
