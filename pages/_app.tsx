cbad740ddb678a3fb43af19de00420aec939ecad
ndx218 <ndx218@gmail.com>
2025-05-11 19:26:38 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
 // pages/_app.tsx
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { Toaster } from 'react-hot-toast';
+import SidebarWrapper from '@/components/SidebarWrapper';
+import '@/styles/globals.css';
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
-      <Component {...pageProps} />
+      <div className="flex min-h-screen">
+        <SidebarWrapper />
+        <div className="flex-1 md:pl-[240px] bg-gray-50">
+          <Component {...pageProps} />
+        </div>
+      </div>
     </SessionProvider>
   );
 }
