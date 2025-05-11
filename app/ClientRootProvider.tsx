0138113f0e2136860906ff8f77aee45369f41bc4
ndx218 <ndx218@gmail.com>
2025-05-11 16:59:29 +0800

    Update ClientRootProvider.tsx

--- app/ClientRootProvider.tsx
+++ app/ClientRootProvider.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
+import { Toaster } from 'react-hot-toast'; // ✅ 加入 toast 提示
 import ClientLayout from './ClientLayout';
 
 export default function ClientRootProvider({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
+      <Toaster position="top-center" toastOptions={{ duration: 3000 }} /> {/* ✅ 提示訊息 */}
       <ClientLayout>{children}</ClientLayout>
     </SessionProvider>
   );
