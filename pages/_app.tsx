2e153ec80e03fea534081024e6133bbd7eb982e9
ndx218 <ndx218@gmail.com>
2025-05-12 13:20:35 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-// pages/_app.tsx
+// /pages/_app.tsx
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { Toaster } from 'react-hot-toast';
@@ -8,12 +8,17 @@ import '@/styles/globals.css';
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
-      <div className="flex min-h-screen">
+      <div className="flex min-h-screen bg-gray-50">
+        {/* 📚 左側固定側欄（含手機版浮出） */}
         <SidebarWrapper />
-        <div className="flex-1 md:pl-[240px] bg-gray-50">
+
+        {/* 🧾 主內容區域 */}
+        <main className="flex-1 md:pl-[240px] p-4">
           <Component {...pageProps} />
-        </div>
+        </main>
+
+        {/* 🔔 toast 通知 */}
+        <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
       </div>
     </SessionProvider>
   );
