2771b5f87ecb4af082490bc01db6cd4f40422a21
ndx218 <ndx218@gmail.com>
2025-05-13 15:18:29 +0800

    Update recharge.tsx

--- pages/recharge.tsx
+++ pages/recharge.tsx
@@ -1,4 +1,9 @@
-import RechargeContent from '@/components/RechargeContent';
+// /pages/recharge.tsx
+import dynamic from 'next/dynamic';
+
+const RechargeContent = dynamic(() => import('@/components/RechargeContent'), {
+  ssr: false,
+});
 
 export default function RechargePage() {
   return <RechargeContent />;
