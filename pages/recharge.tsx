be3c19ef091637e188a44cf1d15db6d09d9b0ca9
ndx218 <ndx218@gmail.com>
2025-05-13 15:33:17 +0800

    Update recharge.tsx

--- pages/recharge.tsx
+++ pages/recharge.tsx
@@ -1,6 +1,7 @@
 // /pages/recharge.tsx
 import dynamic from 'next/dynamic';
 
+// ❗️關鍵：用 dynamic 且禁止 SSR
 const RechargeContent = dynamic(() => import('@/components/RechargeContent'), {
   ssr: false,
 });
