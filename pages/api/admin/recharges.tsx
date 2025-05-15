7fd0078f7da9d5adc1b438327cacef59522f1a06
ndx218 <ndx218@gmail.com>
2025-05-15 14:00:48 +0800

    Update recharges.tsx

--- pages/api/admin/recharges.tsx
+++ pages/api/admin/recharges.tsx
@@ -10,8 +10,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         id: true,
         name: true,
         phone: true,
-        imageUrl: true,
         referralCode: true,
+        imageUrl: true,
         createdAt: true,
       },
     });
