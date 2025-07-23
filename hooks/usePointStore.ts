d1ca9d5c94902abc2f588b86b91c24be1b56eb1d
ndx218 <ndx218@gmail.com>
2025-07-24 03:15:24 +0800

    Update usePointStore.ts

--- hooks/usePointStore.ts
+++ hooks/usePointStore.ts
@@ -1,15 +1,16 @@
-// hooks/usePointStore.ts
 import { create } from 'zustand';
 
-interface PointState {
+/** 把 state 介面 export 出來，其他檔案就能拿來注解 */
+export interface PointState {
   credits: number;
-  set   : (n: number) => void;
-  has   : (need: number) => boolean;   // NEW
-  spend : (cost: number) => void;      // NEW
+  set: (n: number) => void;
+  has: (need: number) => boolean;
+  spend: (cost: number) => void;
 }
+
 export const usePointStore = create<PointState>()((set, get) => ({
   credits: 0,
-  set : (n) => set({ credits: n }),
-  has : (need) => get().credits >= need,
-  spend: (c)  => set(s => ({ credits: s.credits - c }))
+  set: (n) => set({ credits: n }),
+  has: (need) => get().credits >= need,
+  spend: (c) => set((s) => ({ credits: s.credits - c })),
 }));
